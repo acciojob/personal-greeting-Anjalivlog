03b9856d88366d4310c1326fbe9b6e6482ed857a
girishyadav <girishayadav.com>
2024-06-05 22:12:41 +0530

    first change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name, setName] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  }
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+    <div className="Appp">
+      <label>Enter your name:</label>
+      <input type="text" onChange={handleChange}></input>
+    </div>
+    {name && <p>Hello, {name}</p>}
     </div>
   )
 }

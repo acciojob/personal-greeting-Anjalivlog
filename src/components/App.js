cdc77ffba25665553b60526f569a98eec9ea0d69
girishyadav <girishayadav.com>
2024-06-10 14:34:54 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,22 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-
 const App = () => {
-
   const [name, setName] = useState('');
-
-  const handleChange = (event) => {
+ 
+  const handleNameChange = (event) => {
     setName(event.target.value);
-  }
-  
+  };
+ 
   return (
-    <div className="App">
-    <div className="Appp">
-      <label htmlFor="name" >Enter your name:</label>
-      <input type="text" id="name" onChange={handleChange}></input>
+    <div>
+      {/*   <p> Enter your name:</p>  */}   
+      <p>
+        <input type="text" value={name} onChange={handleNameChange} />
+      </p>
+      {name && <p>Hello {name}!</p>}
     </div>
-    {name && <p>Hello {name}!</p>}
-    </div>
-  )
-}
-
-export default App
+  );
+};
+ 
+export default App;
